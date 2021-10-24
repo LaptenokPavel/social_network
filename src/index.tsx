7ca68285e1392e010965754c4aa40e07d453d09d
LaptenokPavel <1982pavlap@gmail.com>
2021-10-24 20:30:11 +0300

    "changes 24.10 github HashRouter"

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from "./App";
 import React from "react";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root'))
+    </HashRouter>, document.getElementById('root'))
 
 
 //@ts-ignore
